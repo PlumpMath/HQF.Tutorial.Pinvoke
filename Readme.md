974b02d2b5a7a80e32dc721c9960c8dc53f8d6bf
huoxudong125 <423huo@163.com>
2015-11-18 18:38:11 +0800

    init the Solution

---
+++ Readme.md
@@ -0,0 +1,4 @@
+#HQF.Tutorial.Pinvoke
+
+The Solution is Demo  How to P/Invoke .in another way, How to exposing native to managed.
+
---
+++ SecondApproach/HQF.Tutorial.Pinvoke.CppCliDll/CppCliWorker.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "..\..\NativeLib\HQF.Tutorial.Pinvoke.NativeLib\Worker.h"
+
+namespace CppCliDll
+{
+
+	public ref class CppCliWorker
+	{
+	public:
+		CppCliWorker();
+
+		~CppCliWorker(); // Dispose()
+		!CppCliWorker(); // Finalize()
+
+		void Start();
+		void Stop();
+		void QueueDemoWorkerTasks();
+		void QueueWorkerTask(unsigned int outerLoopCount, unsigned int innerLoopCount);
+		void ExecuteWorkerTask(unsigned int outerLoopCount, unsigned int innerLoopCount);
+		void PrintStatistics();
+		double GetWorkingSeconds();
+
+	private:
+		Worker* m_actualWorker;
+	};
+
+}
